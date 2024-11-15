8d0f508c1a460c36d3f35ccaa62a14293e8ab92e
Adrian20651 <vasv2024@gmail.com>
2024-11-15 11:57:15 -0600

    inicio pt 2

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -2,20 +2,24 @@ import { Component, OnInit,Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { UserService } from '../../services/user.service';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],  // Ensure HttpClientModule is correctly added here
+  imports: [CommonModule, HttpClientModule,NgxPaginationModule],  // Ensure HttpClientModule is correctly added here
   providers: [UserService],                   // Provide UserService if it uses HttpClient
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
   users: any[] = [];
+  currentPage: number = 1;
   constructor(private userService: UserService) {}
   ngOnInit(): void {
   this.userService.getUsers().subscribe(data => {
   this.users = data;
+  
   });
+  
   }}
